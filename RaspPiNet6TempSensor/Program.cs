8f9ced2ba1333bbbd13e53918693f8ad4af76bc1
Kitchen <garrard.kitchen@fujitsu.com>
2022-06-28 08:56:43 +0100

    style: Reformated file

--- RaspPiNet6TempSensor/Program.cs
+++ RaspPiNet6TempSensor/Program.cs
@@ -9,23 +9,24 @@ Console.WriteLine("Press any key to stop the reading");
 Random _random = new Random(2);
 var controller = new GpioController(PinNumberingScheme.Board);
 
-if (controller.IsPinOpen(7)) {
+if (controller.IsPinOpen(7))
+{
     Console.WriteLine("Pin 7 open, so closing");
-    controller.ClosePin(7); 
+    controller.ClosePin(7);
 }
 
 Console.WriteLine("Opening Pin 7");
 controller.OpenPin(7);
 
-controller.RegisterCallbackForPinValueChangedEvent(7, PinEventTypes.Falling, (a,b) =>
-    {
-        Console.WriteLine($"falling: {b.ChangeType.ToString()}");
-    });
+controller.RegisterCallbackForPinValueChangedEvent(7, PinEventTypes.Falling, (a, b) =>
+{
+    Console.WriteLine($"falling: {b.ChangeType.ToString()}");
+});
 
-    controller.RegisterCallbackForPinValueChangedEvent(7, PinEventTypes.Rising, (a, b) =>
-    {
-        Console.WriteLine($"raising: {b.ChangeType.ToString()}");
-    });
+controller.RegisterCallbackForPinValueChangedEvent(7, PinEventTypes.Rising, (a, b) =>
+{
+    Console.WriteLine($"raising: {b.ChangeType.ToString()}");
+});
 
 controller.RegisterCallbackForPinValueChangedEvent(7, PinEventTypes.None, (a, b) =>
 {
@@ -40,7 +41,7 @@ Dht(new Dht11(7, PinNumberingScheme.Board, controller));
 void Dht(DhtBase dht)
 {
     while (!Console.KeyAvailable)
-    {        
+    {
         Temperature temperature = default;
         RelativeHumidity humidity = default;
         bool success = dht.TryReadHumidity(out humidity) && dht.TryReadTemperature(out temperature);
@@ -49,7 +50,7 @@ void Dht(DhtBase dht)
 
         //if (dht.IsLastReadSuccessful)
         if (success)
-        {            
+        {
             Console.WriteLine($"Temperature: {temperature.DegreesCelsius:F1}\u00B0C, Relative humidity: {humidity.Percent:F1}% {dht.MinTimeBetweenReads}");
 
             // WeatherHelper supports more calculations, such as saturated vapor pressure, actual vapor pressure and absolute humidity.
@@ -57,11 +58,12 @@ void Dht(DhtBase dht)
                 $"Heat index: {WeatherHelper.CalculateHeatIndex(temperature, humidity).DegreesCelsius:F1}\u00B0C");
             Console.WriteLine(
                 $"Dew point: {WeatherHelper.CalculateDewPoint(temperature, humidity).DegreesCelsius:F1}\u00B0C");
-        }else
-        {
-            Console.Write(".");            
         }
-        
+        else
+        {
+            Console.Write(".");
+        }
+
         //Thread.Sleep(_random.Next(100,5000));
         Thread.Sleep(2500);
     }
